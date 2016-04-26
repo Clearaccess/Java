0d0cff91ee9e3f08b28c19c3a3c2fd6c4f4c314f
isaikin <IsAikinV1995>
2016-04-26 12:27:29 +0300

    ADD dao

--- Task4. Generation text/GenerationText/GenerationText.DAL.Interface/IGenerationDAO.cs
+++ Task4. Generation text/GenerationText/GenerationText.DAL.Interface/IGenerationDAO.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace GenerationText.DAL.Interface
+namespace GenerationText.DAL
 {
     public interface IGenerationDAO
     {
-        IDictionary<string, List<string>> GetWords();
+        IDictionary<string, List<string>> Getwords();
     }
 }
--- Task4. Generation text/GenerationText/GenerationText.DAL/GenerationDAO.cs
+++ Task4. Generation text/GenerationText/GenerationText.DAL/GenerationDAO.cs
@@ -4,26 +4,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace GenerationText.DAL
 {
-    public class GenerationDAO: IGenerationDAO
+    public class GenerationDAO
     {
         private static IDictionary<string,List<string>> words;
          
-        static GenerationDAO()
+        public GenerationDAO()
         {
-            words = new Dictionary<string, List<string>>();
+            words = this.GetWords();
         }
 
-        private void ParseFile()
+        public IDictionary<string, List<string>> Getwords()
         {
-
+            return new Dictionary<string, List<string>>(words);
         }
 
-        public IDictionary<string,List<string>> GetWords()
+        private Dictionary<string,List<string>> GetWords()
         {
-            return;
+            if (!File.Exists("inputWords.txt"))
+            {
+                File.Create("inputWords.txt");
+            }
+
+            using (StreamReader input = new StreamReader("inputWords.txt"))
+            {
+                var result = new Dictionary<string, List<string>>();
+
+                while (!input.EndOfStream)
+                {
+                    var tempstring = input.ReadLine().Split(' ');
+
+                    result.Add(tempstring[0], tempstring.Skip(1).ToList());         
+                }
+
+                return result;
+            }
+        }
+
+        private void SaveWords()
+        {
+            using (StreamWriter output = new StreamWriter("inputWords.txt"))
+            {
+                foreach (var word in words)
+                {
+                    output.WriteLine(word);
+                }
+            }
         }
     }
 }
